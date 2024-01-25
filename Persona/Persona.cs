fa06671fc49bc34d8751777606823960a0540534
Riccardo Nervosi <riccardo.nervosi@gmail.com>
2024-01-25 22:49:14 +0100

    Aggiungere i file di progetto.

---
+++ Persona/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int AnnoDiNascita { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} ", Nome, Cognome);
+        }
+
+    }
+}
---
+++ Persona/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            Console.WriteLine(p);
+        }
+            
+    }
+}
